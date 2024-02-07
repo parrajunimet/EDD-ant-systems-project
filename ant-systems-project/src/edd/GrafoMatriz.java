5d8fb3b2934260a4ce829b5dffb6e65da848ac9f
ParraJuanPablo <parrfjp@gmail.com>
2024-02-06 20:24:39 -0400

    #24 Crear Matriz

---
+++ ant-systems-project/src/edd/GrafoMatriz.java
@@ -0,0 +1,122 @@
+package edd;
+
+public class GrafoMatriz {
+    int numVerts;
+    static int MaxVerts = 20;
+    Vertice verts[];
+    double [][] matAd;
+
+    public GrafoMatriz() {
+        matAd = new double [MaxVerts][MaxVerts];
+        verts = new Vertice [MaxVerts];
+        for (int i=0; i< MaxVerts; i++)
+            for (int j=0; j< MaxVerts; j++)
+                matAd[i][j] = 0;
+        numVerts = 0;
+    }
+    
+    public void nuevoVertice (String nom){
+        boolean esta = numVertice(nom) >= 0;
+        if(!esta)
+        {
+            Vertice v = new Vertice(nom);
+            v.setNumVertice(numVerts);
+            verts[numVerts++] = v;
+            System.out.println(numVerts);
+        }
+    }
+    
+    public void eliminarVertice (String nom){
+        boolean esta = numVertice(nom) >= 0;
+        if(esta){
+            Vertice v = new Vertice(nom);   
+            boolean encontrado = false;
+            int i = 0;
+            for(; (i < numVerts) && !encontrado;){
+                encontrado = verts[i].equals(v);
+                if (!encontrado) i++ ;
+            }
+            for (int j = i; j < (MaxVerts-1); j++) {
+                verts[j] = verts[j+1];
+                for (int k = 0; k < (MaxVerts-1); k++) {
+                    matAd[j][k] = matAd[j+1][k];
+                    matAd[k][j] = matAd[k][j+1];
+                }                   
+            }
+            verts[MaxVerts-1] = null;
+            for (int j = 0; j < (MaxVerts-1); j++) {
+                matAd[j][MaxVerts-1] = 0;
+                matAd[MaxVerts-1][j] = 0;
+            }
+            matAd[MaxVerts-1][MaxVerts-1] = 0;
+            numVerts --;
+        }
+    }
+    
+    private int numVertice(String vs){
+        Vertice v = new Vertice(vs);
+        boolean encontrado = false;
+        int i = 0;
+        for(; (i < numVerts) && !encontrado;){
+            if(verts[i] != null){            
+                encontrado = verts[i].equals(v);
+            }
+            if (!encontrado) i++ ;
+        }
+        return (i < numVerts) ? i : -1;
+    }
+    
+    public void nuevoArco(String a, String b, double d)throws Exception{
+        int va, vb;
+        va = numVertice(a);
+        vb = numVertice(b);
+        if(va < 0 || vb <0)throw new Exception ("Vertice no existe");
+        matAd[va][vb] = d;
+        matAd[vb][va] = d;
+    }
+    
+    public boolean adyacente(String a, String b)throws Exception{
+        int va, vb;
+        va = numVertice(a);
+        vb = numVertice(b);
+        if(va < 0 || vb <0)throw new Exception ("Vertice no existe");
+        return matAd[va][vb] != 0;
+    }
+    
+    public ListaDoble adyacentes(String a)throws Exception{
+        int va;
+        ListaDoble adyacentes = new ListaDoble();
+        va = numVertice(a);     
+        if(va < 0)throw new Exception ("Vertice no existe");
+        for (int i = 0; i < MaxVerts; i++) {
+            if(matAd[va][i] != 0){
+                adyacentes.insertFinal(matAd[va][i]);
+            }
+        }
+        return adyacentes;
+    }
+    
+    public void print(){
+        System.out.print("|");
+        
+        for (int y=0; y < verts.length; y++) {
+          if (verts[y] != null){
+              System.out.print (verts[y].nombre);          
+          }
+          if (y!=19) System.out.print("\t");
+        }
+        System.out.println("|");
+        for (int x=0; x < matAd.length; x++) {
+            if (verts[x] != null){
+                System.out.print (verts[x].nombre);          
+            }
+            System.out.print("|");
+            for (int y=0; y < matAd[x].length; y++) {
+              System.out.print (matAd[x][y]);
+              if (y!=matAd[x].length-1) System.out.print("\t");
+            }
+        System.out.println("|");
+}
+    }
+}
+
